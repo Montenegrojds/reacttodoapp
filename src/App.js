e6787ea9740dd67e6a262c17cbabf4b8b18b56d4
monte <montenegrojds@gmail.com>
2021-04-22 17:55:39 -0500

    todolist structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Todolist from './components/todolist'
 
-
 class App extends React.Component {
   render(){
     return (
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
 class Todolist extends React.Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            task: '',
+            tasks: [
+                
+                {taskName: 'test1', id:1},
+                {taskName: 'test2', id:2},
+                {taskName: 'test3', id:3},
+                
+            ]
+        }
+        this.DisplayList = this.DisplayList.bind(this);
+    }
+    
+    DisplayList() {
+        return this.state.tasks.map(name=> {
+            return (
+                <li>
+                    {name.taskName}   
+                </li>
+            )
+        })
+    }
     render(){
         return(
             <div>
-                <h1></h1>
+                <h2>Add new tasks</h2>
+                <form>
+                    <input />
+                    <button>
+                        Add 
+                    </button>
+                </form>
+                <ul>
+                    {this.DisplayList()}
+                </ul>
             </div>
         )
     }
